8617ced734b5e940ddae054642269780e564d30d
nadirali64 <solanginadir693@gmail>
2026-06-17 17:23:37 +0500

    code is updated

--- task1.js
+++ task1.js
@@ -1,25 +1,25 @@
-let num1=prompt("write any number")
-let opr=prompt("write any operator")
-let num2=prompt("write any number")
-num1=Number(num1)
-num2=Number(num2)
-switch(opr){
+let num1 = prompt("write any number")
+let opr = prompt("write any operator")
+let num2 = prompt("write any number")
+num1 = Number(num1)
+num2 = Number(num2)
+switch (opr) {
     case '+':
-    console.log("addition is",num1+num2);
-    break
+        console.log("addition is", num1 + num2);
+        break
     case '-':
-    console.log("subtraction is",num1-num2);
-    break
+        console.log("subtraction is", num1 - num2);
+        break
     case '*':
-    console.log("multiplication is",num1*num2);
-    break
+        console.log("multiplication is", num1 * num2);
+        break
     case '/':
-    console.log("division is",num1/num2);
-    break
+        console.log("division is", num1 / num2);
+        break
     case '%':
-    console.log("remainder is",num%num2);
-    break
+        console.log("remainder is", num % num2);
+        break
     default:
-    console.log("invaild number");
-    
+        console.log("invaild number");
+
 }
