d4b19968f696c3662f65a5013586a10e65dfb0b3
nadirali64 <solanginadir693@gmail>
2026-06-17 17:19:16 +0500

    first commit

---
+++ task.js
@@ -0,0 +1,45 @@
+let month=prompt("enter any number from 1 to 12")
+month=Number(month)
+
+
+switch(month){
+    case 1:
+    console.log("january");
+    break
+    case 2:
+        console.log("feberury");
+    break
+    case 3:
+        console.log("march");
+    break 
+    case 4:
+        console.log("april");
+    break 
+    case 5:
+        console.log("mai");
+    break 
+    case 6:
+        console.log("june");
+    break
+    case 7:
+        console.log("julai");
+    break 
+    case 8:
+        console.log("august");
+    break 
+    case 9:
+        console.log("septomber");
+    break 
+    case 10:
+        console.log("actomber");
+     break             
+    case 11:
+        console.log("november");
+    break 
+    case 12:
+        console.log("december");
+    default:
+        console.log("invaild number");
+        
+                
+}
---
+++ task3.js
@@ -0,0 +1,25 @@
+let msg=prompt("write any string from 1 to 5")
+msg=Number(msg)
+switch(msg){
+    case 1:
+        console.log(" Ecellent");
+    break    
+    case 2:
+        console.log("Very good");
+    break
+    case 3:
+        console.log("Good");
+    break
+    case 4:
+        console.log("Needs Improvement");
+    break
+    case 5:
+        console.log("failed");
+    break
+    default:
+        console.log("invaild grade");
+        
+                
+           
+        
+}
